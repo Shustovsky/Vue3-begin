d5bba811a084ec79aaa7180dcc0f393001154968
A.Shustovsky <a.shustovsky@gmail.com>
2023-12-05 16:51:50 +0400

    feat: add methods computed styles and classes

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const App = {
   data() {
     return {
-      counter: 0,
-      title: 'Счетчик'
+      title: 'Список заметок',
+      placeholderString: 'Введите название заметки',
+      inputValue: '',
+      notes: []
+    }
+  },
+  methods: {
+    addNewNote() {
+      if (this.inputValue) {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
+    },
+    toUpperCase(item) {
+      return item.toUpperCase()
+    },
+    removeNote(index, event) {
+      this.notes.splice(index, 1)
+      console.log(event)
+    },
+    // inputKeyPress(event){
+    //   if (event.key === 'Enter'){
+    //     this.addNewNote()
+    //   }
+    // }
+  },
+  computed: {
+    doubleCountComputed() {
+      return this.notes.length * 2
+    },
+  },
+  watch: {
+    inputValue(value) {
+      console.log(value)
+    },
+    // !!!!!!!!!Не работает почему то((((
+    notes(value) {
+      console.log('notes', value)
     }
   }
 }
