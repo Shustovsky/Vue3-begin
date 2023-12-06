bcdc1e4b2ab5a3f475050262b458102f8e2bc197
A.Shustovsky <a.shustovsky@gmail.com>
2023-12-06 15:01:19 +0400

    feat: vue-introduction

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
+const TodoItem = {
+  props: ['todo', 'index'],
+  template: `<li>{{ index + 1 }}. {{ todo.text }}</li>`
+}
+
 const App = {
+  components:{
+    TodoItem
+  },
   data() {
     return {
       counter: 0,
-      title: 'Счетчик'
+      myTitle: 'Счётчик:',
+      boolean: true,
+      message: 'Привет, Vue.js!',
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
     }
+  },
+  methods: {
+    toggleBoolean() {
+      this.boolean = !this.boolean
+    },
+    reverseMessage() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('')
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
   }
 }
 
-const app = Vue.createApp(App)
-app.mount('#root')
+Vue.createApp(App).mount('#root')
