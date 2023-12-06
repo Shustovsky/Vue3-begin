a3d9b264b8394ae3919b60f8d429198884805b30
A.Shustovsky <a.shustovsky@gmail.com>
2023-12-06 10:28:59 +0400

    feat

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const App = {
-  data() {
-    return {
-      counter: 0,
-      title: 'Счетчик'
+Vue.createApp({
+  data: () => ({
+    myHtml: '<h1>Vue 3</h1>',
+    title: 'Title',
+    person: {
+      firstName: 'Sanya',
+      lastName: 'Shustovsky',
+      age: 30
+    },
+    items: [1, 2]
+  }),
+  methods: {
+    addItem() {
+      this.items.unshift(this.$refs.myInput.value)
+      this.$refs.myInput.value = ''
+    },
+    removeItem(index) {
+      this.items.splice(index, 1)
+    },
+    log(item) {
+      console.log(item)
+    }
+  },
+  computed: {
+    evenItems() {
+      return this.items.filter(i => i % 2)
     }
   }
-}
-
-const app = Vue.createApp(App)
-app.mount('#root')
+}).mount('#root')
